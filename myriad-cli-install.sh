7183a648ca17aa5d00eaf016d22ae6bc1f3693f0
Pandu Sastrowardoyo <1@decentri.city>
2023-05-19 08:17:37 +0700

    Update myriad-cli-install.sh

--- myriad-cli-install.sh
+++ myriad-cli-install.sh
@@ -7,13 +7,13 @@ if ! command -v wget &>/dev/null; then
     sudo apt-get install wget
 fi
 
-# Check if ~/myriad/ directory exists and create it if not
-if [ ! -d "$HOME/myriad" ]; then
-  mkdir $HOME/myriad
+# Check if ~/myriadclient/ directory exists and create it if not
+if [ ! -d "$HOME/myriadclient" ]; then
+  mkdir $HOME/myriadclient
 fi
 
-# Change into the ~/myriad/ directory
-cd $HOME/myriad
+# Change into the ~/myriadclient/ directory
+cd $HOME/myriadclient
 
 # Download files
 wget https://raw.githubusercontent.com/Decentricity/myriad-cli/main/myriad-cli
@@ -24,10 +24,9 @@ chmod +x myriad-cli
 chmod +x myriad-cli.py
 
 # Add the directory to PATH in .bashrc if it's not already there
-grep -qxF 'export PATH=$HOME/myriad:$PATH' $HOME/.bashrc || echo 'export PATH=$HOME/myriad:$PATH' >> $HOME/.bashrc
+grep -qxF 'export PATH=$HOME/myriadclient:$PATH' $HOME/.bashrc || echo 'export PATH=$HOME/myriadclient:$PATH' >> $HOME/.bashrc
 
 # Source .bashrc to update the current shell
 source $HOME/.bashrc
 
-echo "myriad-cli is now installed. You can run it with the command: myriad-cli."
-echo "If this does not work, close and reopen your terminal session."
+echo "myriad-cli is now in your path. You can run it with the command: myriad-cli"
